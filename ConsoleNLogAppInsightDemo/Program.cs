a6191763f55b246dbccaf1f98917a6e4e8fd988c
Udaybhanu Karmakar <udaybhanu.karmakar@in.ibm.com>
2020-05-15 18:18:32 -0400

    Added initial code

---
+++ ConsoleNLogAppInsightDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleNLogAppInsightDemo
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Test Log
+                Logger.Info("Hello world");
+                Logger.Trace("This should go to App insight");
+                Logger.Warn("Wow!! This is a warning message");
+                System.Console.ReadKey();
+                int i = 10;
+
+                Console.WriteLine(i / 0);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Hmm! Something is not right.");
+            }
+        }
+    }
+}
---
+++ MyDotNetClassLibrary/CustomTimeZoneTimeSource .cs
@@ -0,0 +1,39 @@
+﻿using NLog.Time;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleNLogAppInsightDemo.MyDotNetClassLibrary
+{
+    [TimeSource("CustomTimeZone")]
+    public class CustomTimeZoneTimeSource : TimeSource
+    {
+        string ZoneName;
+        TimeZoneInfo ZoneInfo;
+
+        [Required]
+        public string Zone
+        {
+            get { return ZoneName; }
+            set
+            {
+                ZoneName = value;
+                ZoneInfo
+                    = TimeZoneInfo.FindSystemTimeZoneById(value);
+            }
+        }
+
+        public override DateTime Time
+        {
+            get
+            {
+                return TimeZoneInfo.ConvertTimeFromUtc(
+                    DateTime.UtcNow, ZoneInfo);
+            }
+        }
+
+        public override DateTime FromSystemTime(DateTime systemTime)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(systemTime, ZoneInfo);
+        }
+    }
+}
